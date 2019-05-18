7f23e041b9ccd820cd47c0d2de97673ff21f0baa
Chengbin <kfengc27@gmail.com>
2019-05-18 20:18:15 +1000

    add files

--- account/urls.py
+++ account/urls.py
@@ -6,11 +6,6 @@ from django.contrib.auth.views import logout_then_login
 from . import views  
 
 urlpatterns = [
-    # post views
-    # url(r'', views.user_login, name='login'),
-    # url(r'^social-auth/', include('social.apps.django_app.urls', namespace='social')),
-    # url(r'^login/$', views.user_login, name = "login"),
-    #login / logou urls 
     url(r'^login/$',
         LoginView.as_view(),
         name='login'),
@@ -19,13 +14,6 @@ urlpatterns = [
         name='logout'),
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
     url(r'^$', views.dashboard, name='dashboard'),
-
-    # url(r'^logout-then-login/$',
-    #     logout_then_login.views(),
-    #     name='logout_then_login'),
-
-    # url(r'^logout/$', 'django.contrib.auth.views.logout', name ='logout'),
-    # url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
     
 ]
 
